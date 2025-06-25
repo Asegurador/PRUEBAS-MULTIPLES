eb2e021767f90b3c7f7cb796f44ebc59e8f8f03f
Asegurador <co0678asegurador@gmail.com>
2025-06-25 14:40:32 -0500

    Update animaciones.js

--- animaciones.js
+++ animaciones.js
@@ -2,7 +2,7 @@
 // Protecciones avanzadas para webs
 // Archivo: seguridad_pro.js
 // =============================
-
+ 
 // 1. Bloquear clic derecho
 window.addEventListener('contextmenu', e => e.preventDefault());
 
